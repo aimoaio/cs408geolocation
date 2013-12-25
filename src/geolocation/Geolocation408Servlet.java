74dfe39a6ebd658a87657669809851950f6fa398
SeeMai <aimoaio@gmail.com>
2013-12-25 15:30:24 +0000

    Committing all files

---
+++ src/geolocation/Geolocation408Servlet.java
@@ -0,0 +1,11 @@
+package geolocation;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class Geolocation408Servlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
---
+++ src/geolocation/PDFextractTest.java
@@ -0,0 +1,28 @@
+package geolocation;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
+
+
+public class PDFextractTest {
+
+	public static void main(String[] args) {
+		PDDocument document;
+		try {
+			PDFTextStripper stripper;
+			document = PDDocument.load(new File("war\\lecture5.pdf"));
+			stripper = new PDFTextStripper();
+			stripper.setSortByPosition( true );
+			System.out.println(stripper.getText(document));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
